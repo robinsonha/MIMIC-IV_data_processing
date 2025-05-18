99fe6a8ab8bb20ec14cb8051f10474cd55f44b6a
Heather Robinson <126197658+robinsonha@users.noreply.github.com>
2025-05-18 19:52:37 +0100

    Create preeclampsia_observations.R

---
+++ preeclampsia_observations.R
@@ -0,0 +1,10 @@
+cohort<-read.csv("preeclampsia_all.csv")
+
+#omr table is the Online Medical Record 
+#blood pressure, height, weight, body mass index, and Estimated Glomerular Filtration Rate (eGFR). 
+#These values are available from both inpatient and outpatient visits
+
+omr<-read.csv("omr.csv")
+omr<-omr[omr$subject_id %in% cohort$subject_id,]
+
+write.csv(omr,"preeclampsia_observations.csv")
